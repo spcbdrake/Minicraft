12711e05638b028db3f67eab4017caab948447a4
oakes <zsoakes@gmail.com>
2015-10-22 00:02:15 -0400

    Add assets and modify initial code

--- core/src/com/theironyard/minicraft/Minicraft.java
+++ core/src/com/theironyard/minicraft/Minicraft.java
@@ -5,23 +5,37 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Minicraft extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    final int WIDTH = 100;
+    final int HEIGHT = 100;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+    SpriteBatch batch;
+    TextureRegion down, up, right, left;
+
+    float x = 0;
+    float y = 0;
+
+    @Override
+    public void create () {
+        batch = new SpriteBatch();
+
+        Texture tiles = new Texture("tiles.png");
+        TextureRegion[][] grid = TextureRegion.split(tiles, 16, 16);
+        down = grid[6][0];
+        up = grid[6][1];
+        right = grid[6][2];
+        left = grid[6][3];
+    }
+
+    @Override
+    public void render () {
+        Gdx.gl.glClearColor(0, 0.5f, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        batch.draw(down, x, y, WIDTH, HEIGHT);
+        batch.end();
+    }
 }
